bee7e1182d0d1ebc15ec9d34cf6393e7d522bc24
aliqadomi777 <aliqadomi000@gmail.com>
2025-03-22 12:55:03 +0300

    ffff

--- loops/script.js
+++ loops/script.js
@@ -1,9 +1,22 @@
-function fact(x) {
-  result = 1;
-  for (; x > 1; x--) {
-    result = result * x;
-  }
-  return result;
-}
+// function fact(x) {
+//   result = 1;
+//   for (; x > 1; x--) {
+//     result = result * x;
+//   }
+//   return result;
+// }
 
-console.log(fact(5));
+// console.log(fact(5));
+
+// function isPalindrome(x) {
+//   for (let y = 0; y < x.length / 2; y++) {
+//     if (x[y] !== x[x.length - 1 - y]) return false;
+//   }
+//   return true;
+// }
+
+// function isPalindrome(x) {
+//   return x === x.split("").reverse().join("");
+// }
+
+
