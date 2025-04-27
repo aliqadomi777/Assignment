a483940e98fe414995f2a0b1afb522b5dfc22d4e
aliqadomi777 <aliqadomi000@gmail.com>
2025-04-27 16:53:23 +0300

    ddd

---
+++ Backend/lec5.1/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  const numberOfLetters = req.body["fname"].length + req.body["lname"].length;
+  res.render("index.ejs", { numberOfLetters: numberOfLetters });
+});
+app.listen(port, () => {});
